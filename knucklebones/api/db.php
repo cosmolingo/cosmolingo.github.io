207003bc382a8e77cf6fbddaef1b8c7e024a4403
titanbin <tristan.boin@obspm.fr>
2025-10-07 22:47:52 +0200

    Update db.php

--- knucklebones/api/db.php
+++ knucklebones/api/db.php
@@ -1,12 +1,14 @@
 <?php
 include('/var/www/creds.php');
 
+$DB_NAME = 'knucklebones';
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 try {
-  $pdo = new PDO("mysql:host={$servername};dbname={$dbname};charset=utf8mb4", $username, $password, [
+  $pdo = new PDO("mysql:host={$servername};dbname={$DB_NAME};charset=utf8mb4", $username, $password, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
   ]);
