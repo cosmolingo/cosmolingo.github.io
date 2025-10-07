527d5bd03cc178627072875f3a8821705293545a
titanbin <tristan.boin@obspm.fr>
2025-10-07 22:40:35 +0200

    Update db.php

--- knucklebones/api/db.php
+++ knucklebones/api/db.php
@@ -1,12 +1,8 @@
 <?php
-// Edit these values for your environment
-$DB_HOST = '127.0.0.1';
-$DB_NAME = 'knucklebones';
-$DB_USER = 'root';
-$DB_PASS = 'RandoDumbai18';
+include('/var/www/creds.php');
 
 try {
-  $pdo = new PDO("mysql:host={$DB_HOST};dbname={$DB_NAME};charset=utf8mb4", $DB_USER, $DB_PASS, [
+  $pdo = new PDO("mysql:host={$servername};dbname={$DB_NAME};charset=utf8mb4", $username, $password, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
   ]);
