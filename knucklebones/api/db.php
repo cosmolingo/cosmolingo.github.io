237ca97652f1745adc0a982392c434b3e5d8c2a2
titanbin <tristan.boin@obspm.fr>
2025-10-07 22:47:02 +0200

    Update db.php

--- knucklebones/api/db.php
+++ knucklebones/api/db.php
@@ -6,7 +6,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 try {
-  $pdo = new PDO("mysql:host={$servername};dbname={$DB_NAME};charset=utf8mb4", $username, $password, [
+  $pdo = new PDO("mysql:host={$servername};dbname={$dbname};charset=utf8mb4", $username, $password, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
   ]);
