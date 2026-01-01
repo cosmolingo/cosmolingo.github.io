716fd4ac9bc67d8588fab9e638a42d6263e40aca
titanbin <tristan.boin@obspm.fr>
2026-01-01 11:22:56 +0100

    added random word page

---
+++ php/get_random_word.php
@@ -0,0 +1,24 @@
+<?php
+include('/var/www/creds.php');
+include('functions.php');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$seed = date('z');
+
+$sql = "SELECT * FROM words
+        ORDER BY RAND($seed)
+        LIMIT 1
+        ";
+$result = $conn->query($sql);
+$word = $result->fetch_assoc();
+
+header('Content-Type: application/json');
+echo json_encode($word);
+?>
