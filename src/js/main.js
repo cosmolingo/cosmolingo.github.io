38423028ed7eef67dab108bc42c36f4df9ad1554
titanbin <tristan.boin@obspm.fr>
2026-04-09 11:13:41 +0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -85,7 +85,7 @@ async function setup_all() {
 }
 
 $(document).ready(() => {
-    console.log("readyy");
+    console.log("ready");
     section = getUrlParameter('section');
     if (window.location.pathname.endsWith("index.html")) {
         const newPath = window.location.pathname.replace("index.html", "");
